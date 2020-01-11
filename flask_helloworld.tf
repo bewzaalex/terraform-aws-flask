54b9b8626e3ee75a376d2eb9dc5f86a07c16322e
abevza <abevza@abevza-mac.localdomain>
2020-01-12 01:53:52 +0200

    Initial commit

---
+++ flask_helloworld.tf
@@ -0,0 +1,84 @@
+variable "project_dir" {
+    default = "/srv/http/flask_helloworld"
+}
+
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-1"
+}
+
+resource "aws_key_pair" "flask_helloworld" {
+  key_name = "flask_helloworld_key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "flask_helloworld_ec_001" {
+  ami           = "ami-2757f631" # Ubuntu 16.04.2 LTS
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.flask_helloworld.key_name
+
+  connection {
+    type    = "ssh"
+    user    = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    host    = self.public_ip
+  }
+
+  # Install packages
+  provisioner "remote-exec" {
+    script = "provision/install_packages.sh"
+  }
+
+  # Create project dir
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mkdir -p ${var.project_dir}",
+      "sudo chown ubuntu:www-data ${var.project_dir}",
+      "sudo chmod 0775 ${var.project_dir}",
+    ]
+  }
+
+  # Copy project files
+  provisioner "file" {
+    source      = "hello.py"
+    destination = "${var.project_dir}/hello.py"
+  }
+  provisioner "file" {
+    source      = "requirements.txt"
+    destination = "${var.project_dir}/requirements.txt"
+  }
+
+  # Create virtualenv
+  provisioner "remote-exec" {
+    inline = [
+      "cd ${var.project_dir}",
+      "virtualenv -p /usr/bin/python3 ./env",
+      "./env/bin/pip install -r requirements.txt",
+    ]
+  }
+
+  # Start Flask
+  provisioner "remote-exec" {
+    inline = [
+      "cd ${var.project_dir}",
+      "./env/bin/python hello.py &",
+    ]
+  }
+
+  # Copy nginx config
+  provisioner "file" {
+    source      = "confs/nginx/flask_helloworld.conf"
+    destination = "/tmp/flask_helloworld.conf"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mv /tmp/flask_helloworld.conf /etc/nginx/sites-available/flask_helloworld.conf",
+      "sudo ln -s /etc/nginx/sites-available/flask_helloworld.conf /etc/nginx/sites-enabled/flask_helloworld.conf",
+      "sudo nginx -t && sudo service nginx restart",
+    ]
+  }
+}
+
+output "flask_helloworld_ip" {
+  value = aws_instance.flask_helloworld_ec_001.public_ip
+}
